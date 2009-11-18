089850685c5c4ba27e8ebfd9f71623fdabdb21a2
Ruslan Yakushev <ruslany@php.net>
2009-11-18 07:07:21 +0000

    Cleaned up the formatting of the comments for the configuration section in wincache.php, so that it looks nice in PHP editors.

--- wincache.php
+++ wincache.php
@@ -42,17 +42,21 @@ define('USE_AUTHENTICATION', 1);
 define('USERNAME', 'wincache');
 define('PASSWORD', 'wincache');
 
-/*The Basic PHP authentication will work only when IIS is configured to support 
-'Anonymous Authentication' and nothing else. If IIS is configured to support/use
-any other kind of authentication like Basic/Negotiate/Digest etc. this will not work.
-In that case please define the name of users in the array below which you would like
-to grant access in your domain/network/workgroup.*/
+/**
+ * The Basic PHP authentication will work only when IIS is configured to support 
+ * Anonymous Authentication' and nothing else. If IIS is configured to support/use
+ * any other kind of authentication like Basic/Negotiate/Digest etc, this will not work.
+ * In that case use the array below to define the names of users in your 
+ * domain/network/workgroup which you want to grant access to.
+ */
 $user_allowed = array('DOMAIN\user1', 'DOMAIN\user2', 'DOMAIN\user3');
 
-/*If the array contains string 'all' all the users authenticated by IIS
-will have access to the page. Uncomment the below line and comment above line
-to grant access to all users who gets authenticated by IIS.*/
-/*$user_allowed = array('all');*/
+/**
+ * If the array contains string 'all', then all the users authenticated by IIS
+ * will have access to the page. Uncomment the below line and comment above line
+ * to grant access to all users who gets authenticated by IIS.
+ */
+/* $user_allowed = array('all'); */
 
 /** ===================== END OF CONFIGURATION SETTINGS ========================== */
 
