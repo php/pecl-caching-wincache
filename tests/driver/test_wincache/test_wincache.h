e633459842d0949732f5455a3c7a78f76421d91f
Venkat Raman Don <donraman@php.net>
2010-01-29 21:59:27 +0000

    Copyright update and some minor updates

--- tests/driver/test_wincache/test_wincache.h
+++ tests/driver/test_wincache/test_wincache.h
@@ -1,3 +1,35 @@
+/*
+   +----------------------------------------------------------------------------------------------+
+   | Windows Cache for PHP                                                                        |
+   +----------------------------------------------------------------------------------------------+
+   | Copyright (c) 2009, Microsoft Corporation. All rights reserved.                              |
+   |                                                                                              |
+   | Redistribution and use in source and binary forms, with or without modification, are         |
+   | permitted provided that the following conditions are met:                                    |
+   | - Redistributions of source code must retain the above copyright notice, this list of        |
+   | conditions and the following disclaimer.                                                     |
+   | - Redistributions in binary form must reproduce the above copyright notice, this list of     |
+   | conditions and the following disclaimer in the documentation and/or other materials provided |
+   | with the distribution.                                                                       |
+   | - Neither the name of the Microsoft Corporation nor the names of its contributors may be     |
+   | used to endorse or promote products derived from this software without specific prior written|
+   | permission.                                                                                  |
+   |                                                                                              |
+   | THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS  |
+   | OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF              |
+   | MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE   |
+   | COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,    |
+   | EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE|
+   | GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED   |
+   | AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING    |
+   | NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED |
+   | OF THE POSSIBILITY OF SUCH DAMAGE.                                                           |
+   +----------------------------------------------------------------------------------------------+
+   | Module: test_wincache.h                                                                      |
+   +----------------------------------------------------------------------------------------------+
+   | Author: Don Raman <donraman@microsoft.com>                                                   |
+   +----------------------------------------------------------------------------------------------+
+*/
 #include "Exdisp.h"
 #include <comutil.h>
 #include <iostream>
@@ -16,8 +48,6 @@ class TestWinCache
     unsigned int total;
     unsigned int passed;
     string logPath;
-    static const int realtotal = 10;
-
     void dumpHeader();
     string getLogFile(string physicalPath);
     bool log(string file, string str);
@@ -43,10 +73,6 @@ public:
         logEntry += "\r\n";
         logEntry += "Total test cases passed: ";
         sprintf(buf, "%d", passed);
-        logEntry += buf;
-        logEntry += "\r\n";
-        logEntry += "Expected Test to run: ";
-        sprintf(buf, "%d", realtotal);
         logEntry += buf;
         logEntry += "\r\n\r\n";
         string file = getLogFile(logPath);
